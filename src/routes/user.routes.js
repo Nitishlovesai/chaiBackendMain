8d19b1acea71fb993794fe532693391b26f0e339
Nitishlovesai <140372763+Nitishlovesai@users.noreply.github.com>
2025-12-02 22:11:50 +0530

    Implement refresh token functionality with JWT verification and route integration

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { loginUser, logoutUser, registerUser } from "../controllers/user.controller.js";
+import { loginUser, logoutUser, registerUser ,refreshAccessToken } from "../controllers/user.controller.js";
 
 import {upload} from "../middlewares/multer.middlewares.js"
 import { verifyJWT } from "../middlewares/auth.middlewares.js";
@@ -25,5 +25,7 @@ router.route("/login").post(loginUser)
 //secured Routes
 //dhayan do tum jitne chaho utne middleware laga sakete ho likw varifyJWT 
 router.route("/logout").post(verifyJWT, logoutUser)
+
+router.route("/refresh-token").post(refreshAccessToken)
 
 export default router
